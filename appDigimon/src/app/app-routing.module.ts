91e5563eea032f2ca55cecac666e0961901f8837
Wollace-Buarque <loremaster966@gmail.com>
2023-12-04 23:49:46 -0300

    ended last page

--- appDigimon/src/app/app-routing.module.ts
+++ appDigimon/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/page1', pathMatch: 'full'},
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
   { path: 'page3', component: Page3Component },
